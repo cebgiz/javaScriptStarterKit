508b3f92e49d80093ffe59025df533a80a59f02a
cengizhan kaya <berat.cengizhan.kaya.bal@gmail.com>
2021-10-19 00:02:30 +0300

    18.10.2021

---
+++ src/app.js
@@ -0,0 +1,46 @@
+console.log("cengizhan kaya")
+
+    //js de double int önemi yok 
+   
+    let dolarBugun = 9.30
+    let dolarDun = 9.20
+
+    {
+        let dolarDun = 10
+
+
+
+
+    }
+
+
+
+    console.log(dolarDun)
+
+    const euroDun = 11
+    
+    
+    //array
+
+    let konutKredileri= ["Konut Kredisi", "Emlak Konut Kredisi","Kamu Komut Kredisi","bilmemne kredisi"]
+
+    console.log("<ul>")
+    
+
+    for(let i = 0; i<konutKredileri.length;i++){
+        console.log("<li> " +konutKredileri[i]+ "</li>")
+
+
+    }
+
+    console.log("</ul>")
+
+    // <ul>
+    //     <li>Konut Kredisi </li>
+    //     <li>Emlak Konut Kredisi </li>
+    //     <li>Kamu Konut Kredisi </li>
+
+
+
+    // </ul>
+    
